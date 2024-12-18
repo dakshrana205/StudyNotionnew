0a9681587be3c06d50186795d6700c9b3e6866a1
Daksh rana <ranadaksh205@gmail.com>
2024-12-19 01:53:57 +0530

    link

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "*",
+		origin: "https://study-notionnew.vercel.app",
 		credentials: true,
 	})
 );
--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL || "https://studynotionnew.onrender.com/api/v1";
+const BASE_URL = "https://studynotionnew.onrender.com/api/v1";
 console.log("Base URL:", BASE_URL);
 
 // AUTH ENDPOINTS
