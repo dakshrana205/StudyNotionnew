2095d47d14ac007769ab350fc0df1f4038737bec
Daksh rana <ranadaksh205@gmail.com>
2025-05-21 16:08:49 +0530

    Update CORS config to allow Vercel domain

--- server/index.js
+++ server/index.js
@@ -28,7 +28,8 @@ app.use(cookieParser());
 app.use(
 	cors({
 	  origin: [
-		"http://localhost:3000"
+		"http://localhost:3000",
+		"https://study-notionnew.vercel.app"
 	  ],
 	  credentials: true,  // Allow credentials (cookies, session) to be sent with requests
 	})
